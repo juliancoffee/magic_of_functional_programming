23bbdaa5a1399efc582bc7b9607e69d9799f35db
juliancoffee <lightdarkdaughter@gmail.com>
2023-05-02 10:27:42 +0300

    Add Currying example to OCaml

--- Main.ml
+++ Main.ml
@@ -1,13 +1,23 @@
 (* currying *)
 let f a = fun b -> a + b
 
-(* kind of currying, but not all the time, here we use fun a b, which also
-   expects two arguments 
+(* yet another example of higher-order functions
 
-   if we wanted to write our f in normal way, we would use following
-    let f a b = a + b
-   and it would behave identically, so you could call it like
-    f 5
-   and get function that "rememberes" 5 and expects value to evaluate fully
-   *)
+ this one takes a function as parameter and returns a function that takes two
+ arguments and applies our function to these parameters
+
+ oh, and this resulting function is curried too, even if we don't do anything *)
 let g func = fun a b -> func a b
+
+(* I promised example for currying *)
+open List
+
+(* this is completely identical to our f, but we let compiler do the magic *)
+let sum x y = x + y
+(* yay, we now have x++ from C, except it is sane *)
+let inc = sum 1
+
+let bigger_positives l =
+    l
+    |> List.filter (fun x -> x >= 0) (* this is a function *)
+    |> List.map inc (* this too *)
